1c5d7f9a909f3b929f29407fde5a15b4211804da
Kento_Izumi2 <amanda.batista71@etec.sp.gov.br>
2023-04-25 10:01:51 -0300

    first commit

---
+++ database/migrations/2023_04_24_023119_create_agendamentos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendamentos', function (Blueprint $table) {
+            $table->string("Nome");
+            $table->string("Telefone");
+            $table->string("Origem");
+            $table->string("Data_Contato");
+            $table->string("Observacao");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendamentos');
+    }
+};
